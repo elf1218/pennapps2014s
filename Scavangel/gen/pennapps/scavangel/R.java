ce71fd6da5c374f62d49bba0f6d5cbf0813e54b9
elf1218 <kevinmu01@gmail.com>
2014-02-15 17:01:23 -0500

    create_hunt sucks

--- Scavangel/gen/pennapps/scavangel/R.java
+++ Scavangel/gen/pennapps/scavangel/R.java
@@ -47,6 +47,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int create=0x7f050009;
+        public static final int createhuntsubmit=0x7f05000e;
         public static final int hello_world=0x7f050002;
         public static final int hint=0x7f05000d;
         public static final int hunt=0x7f05000b;
