9616d450997dc410b5d4117510b4153c48f6cf54
Carolyn Lim <carolim@sas.upenn.edu>
2014-02-15 18:57:16 -0500

    join hunt created

---
+++ Scavangel/src/pennapps/scavangel/Join_Hunt.java
@@ -0,0 +1,12 @@
+package pennapps.scavangel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Join_Hunt extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.join_hunt);
+
+	}
+}
