a0e52a51e4fe0f3a8756c89803215c44501eef34
Carolyn Lim <carolim@sas.upenn.edu>
2014-02-15 20:21:07 -0500

    enter location

--- Scavangel/gen/pennapps/scavangel/R.java
+++ Scavangel/gen/pennapps/scavangel/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
+        public static final int RelativeLayout1=0x7f080009;
+        public static final int action_settings=0x7f08000a;
         public static final int button1=0x7f080003;
         public static final int button2=0x7f080002;
         public static final int editText1=0x7f080004;
@@ -39,6 +40,8 @@ public final class R {
         public static final int activity_form=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int create_hunt=0x7f030002;
+        public static final int enter_location=0x7f030003;
+        public static final int join_hunt=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -50,7 +53,10 @@ public final class R {
         public static final int hello_world=0x7f050002;
         public static final int hint=0x7f05000d;
         public static final int hunt=0x7f05000b;
+        public static final int huntsearch=0x7f05000f;
+        public static final int joinhunt=0x7f05000e;
         public static final int location=0x7f05000c;
+        public static final int locationanswer=0x7f050010;
         public static final int login=0x7f050003;
         public static final int participate=0x7f05000a;
         public static final int pwd=0x7f050004;
---
+++ Scavangel/src/pennapps/scavangel/Enter_Location.java
@@ -0,0 +1,11 @@
+package pennapps.scavangel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Enter_Location extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.enter_location);
+	}
+}
