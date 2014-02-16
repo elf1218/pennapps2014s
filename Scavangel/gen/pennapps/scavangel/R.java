9046050d6770e3de1deff91e6df227d00f13166a
elf1218 <kevinmu01@gmail.com>
2014-02-15 22:59:33 -0500

    Background hype!

--- Scavangel/gen/pennapps/scavangel/R.java
+++ Scavangel/gen/pennapps/scavangel/R.java
@@ -21,7 +21,9 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg=0x7f020000;
+        public static final int bgc=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int Button01=0x7f080009;
@@ -29,15 +31,15 @@ public final class R {
         public static final int action_settings=0x7f08000d;
         public static final int button1=0x7f080003;
         public static final int button2=0x7f080002;
-        public static final int editText1=0x7f080004;
-        public static final int editText2=0x7f080006;
+        public static final int editText1=0x7f080005;
+        public static final int editText2=0x7f080004;
         public static final int editText3=0x7f080008;
-        public static final int hint=0x7f08000b;
+        public static final int hint=0x7f08000a;
         public static final int huntid=0x7f080007;
-        public static final int locationguess=0x7f08000a;
+        public static final int locationguess=0x7f08000b;
         public static final int tableRow1=0x7f080000;
         public static final int tableRow2=0x7f080001;
-        public static final int textView1=0x7f080005;
+        public static final int textView1=0x7f080006;
     }
     public static final class layout {
         public static final int activity_form=0x7f030000;
